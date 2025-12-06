7436da0e8be70dee1973cdc38a8b208cbcf32ed5
Erica Abrahms <erica@abrah.ms>
2025-12-06 13:56:17 -0800

    Passes the tests on day 6.  Having trim issues on part 2 while reading the input.
    Was able to manually handle the last couple of columns to validate the response.

--- day06/solution.js
+++ day06/solution.js
@@ -1,6 +1,6 @@
 // Day 6 Solution
 
-function parseInput(input) {
+function parseInputPartOne(input) {
   const rows = input.split('\n')
   return rows.map(row => row.split(' ').filter(item => item))
 }
@@ -9,7 +9,7 @@ export const day06 = {
   part1: (input) => {
     let sumOfSolutions = 0;
 
-    const rows = parseInput(input);
+    const rows = parseInputPartOne(input);
     
     for (let i = 0; i < rows[0].length; i++) {
       let operator = rows[rows.length - 1][i]; 
@@ -30,6 +30,44 @@ export const day06 = {
   }, 
   part2: (input) => {
     let sumOfSolutions = 0;
+
+    const rows = input.split('\n');
+    const operatorsIndex = rows.length - 1;
+    const rowLength = rows[0].length
+
+    let values = [];
+    for (let charIdx = rowLength - 1; charIdx >= 0; charIdx--) {
+      console.log(charIdx);
+
+      let result = 0;
+      let colValue = '';
+
+
+      for (let rowIdx = 0; rowIdx < rows.length; rowIdx++) {
+
+        let currentCharacter = rows[rowIdx][charIdx];
+        if (rowIdx < operatorsIndex) {
+          colValue += currentCharacter;
+        } else {
+          values.push(colValue);
+        if (!currentCharacter.trim()) { continue }
+          else {
+            if (currentCharacter == '+') {
+              result = values.reduce((acc, curr) => {
+                return acc += parseInt(curr.trim() || 0);
+              }, 0)
+            } else {
+              result = values.reduce((acc, curr) => {
+                return acc *= parseInt(curr.trim() || 1);
+              }, 1)
+            }
+            values = []
+            sumOfSolutions += result
+          }
+        }
+      }
+
+    }
 
     return sumOfSolutions;
   }
